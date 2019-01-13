fa3432473c17066f644f236d07052bef2f4e6e4e
tech4GT <varun.gupta1798@gmail.com>
2019-01-13 20:28:30 +0530

    greyscale
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

---
+++ grayscale.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy
+
+img = cv2.imread('/Users/tech4GT/Desktop/Images/photo.jpeg')
+# cv2.imshow('image',img)
+# a = numpy.asarray(img)
+for x in range(img.shape[0]):
+    for y in range(img.shape[1]):
+        val = 0.3*img.item(x, y, 0) + 0.59*img.item(x,
+                                                    y, 1) + 0.11*img.item(x, y, 2)
+        img.itemset((x, y, 0), val)
+        img.itemset((x, y, 1), val)
+        img.itemset((x, y, 2), val)
+        # a[x][y][0] = val
+        # a[x][y][1] = val
+        # a[x][y][2] = val
+# print(a)
+cv2.imshow("image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
