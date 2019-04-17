17d55e28430adf406bc038d8934e7412d24305f8
tech4GT <varun.gupta1798@gmail.com>
2019-04-17 08:45:54 +0530

    add more morphological operations
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

---
+++ opening.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+import dilation as d
+import erosion as e
+
+ip = cv2.imread("./Images/erosion.png")
+img = np.zeros((ip.shape[0], ip.shape[1], 1), np.uint8)
+
+for i in range(ip.shape[0]):
+    for j in range(ip.shape[1]):
+        img.itemset((i, j, 0), 0.3*ip.item(i, j, 2) +
+                    0.59*ip.item(i, j, 1) +
+                    0.11*ip.item(i, j, 0))
+
+se = [(-1, -1), (1, -1), (-1, 1), (1, 1), (-1, 0), (0, -1),
+      (0, 0), (0, 1), (1, 0), (-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2)]
+
+inter = e.erode(img, se)
+img = d.dilate(inter, se)
+cv2.imshow('input', ip)
+cv2.imshow('intermediate', inter)
+cv2.imshow('output', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
